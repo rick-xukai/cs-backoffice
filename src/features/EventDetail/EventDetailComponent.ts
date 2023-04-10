f058cc2157bc2ca93b9052681b937ad1c1037c28
rick.xu <rick.xu@imaginato.com>
2023-04-10 11:23:44 +0800

    fix NFT image issue

--- src/features/EventDetail/EventDetailComponent.ts
+++ src/features/EventDetail/EventDetailComponent.ts
@@ -153,6 +153,9 @@ const TicketTypesContainer = styled.div`
       color: ${Colors.black5};
     }
   }
+  .ant-upload-list-item-name {
+    color: ${Colors.grey6};
+  }
   /* background: ${Colors.white};
   box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.04);
   border-radius: 4px;
